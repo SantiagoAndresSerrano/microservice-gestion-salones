70c357de85ece956d5b58fb69baa73c8740f5e77
dilansleytfm <gedersongustavogbot@ufps.edu.co>
2022-11-14 13:05:43 -0500

    Avance forms

--- src/main/resources/static/js/function.js
+++ src/main/resources/static/js/function.js
@@ -1,26 +1,37 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-    chargeInventory();
-    $('#inventory').DataTable();
-});
+add();
+//$select.addEventListener("change", opcionCambiada);
 
-async function chargeInventory(){
-
-    const request = await fetch('api/inventory', {
+async function add(){
+    const request = await fetch('actividad', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
     });
-    const inventory = await request.json();
-
-    let listHTML = '';
-    for (let data of inventory){
-        //<td><a href="#" onclick="" class="btn btn-primary"></a></td>
-        let inventoryHTML = '<tr><td>'+data.id_inventario+'</td><td>'+data.nombre+'</td></tr>';
-        listHTML += inventoryHTML;
+    const actividades = await request.json();
+    let msg = document.querySelector("#cmbActividad");
+    for (let i=0; i < actividades.length ; ++i){
+        msg.innerHTML += `<option value="${actividades[i].id_actividad}">${actividades[i].nombre}</option>`;
     }
+}
 
-    document.querySelector("#inventory tbody").outerHTML = listHTML;
+async function registrarPrestamo(){
+    let data = {};
+
+    data.id_actividad = document.getElementById('cmbActividad').value;
+    data.fecha_inicio = document.getElementById('horaInicio').value;
+    data.fecha_fin = document.getElementById('horaFin').value;
+
+    //Algoritmo validacion i guess
+    const request = await fetch('prestamo', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    window.location.href = 'other.html';
 }
